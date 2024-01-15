737765c8fb772793e91b81171f8d5ac3bbf368ce
allenamaya <ahlee.amaya@gmail.com>
2024-01-15 23:26:50 +0300

    ✨FRONTEND AUTHENTICATION INTERPHASE

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './compontents/Login /Login';
+import Signup from './compontents/signup/Signup';
+export const App = () => {
+  return (
+    <div>
+<Login/>
+<Signup/>
+    </div>
+  )
+}
+
+export default App;
